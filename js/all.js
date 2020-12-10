f61574486e6303af58117139766cd7a579ba3fb1
Logan <luologan19@gmail.com>
2020-12-10 20:15:13 +0800

    add video billboard/animated/scss file

--- js/all.js
+++ js/all.js
@@ -1,10 +1,60 @@
 $(document).ready(function () {
-    $('a[data-dismiss="modal"][data-toggle="modal"]').on('click',function(){
-        var target = $(this).data('target');
-        $(target).on('shown.bs.modal' ,function(){
+    // modal
+    $('a[data-dismiss="modal"][data-toggle="modal"]').on('click', function () {
+        const target = $(this).data('target');
+        $(target).on('shown.bs.modal', function () {
             $('body').addClass('modal-open');
         });
     });
+    // menu fade
+    $('.menu li').hover(function () {
+        $(this).find('.cnTitle').addClass('animate__fadeInRightBig');
+        $(this).find('.cnTitle').removeClass('animate__fadeOutRightBig');
+    },
+        function () {
+            $(this).find('.cnTitle').addClass('animate__fadeOutRightBig');
+            $(this).find('.cnTitle').removeClass('animate__fadeInRightBig');
+
+        }
+    );
+
+    //scroll nav
+    $('.scrollNav').click(function (e) {
+        e.preventDefault()
+        const target = $(this).attr('href');
+        const targetPos = $(target).offset().top - 61;
+        $('html,body').animate({ scrollTop: targetPos }, 1000);
+    })
+
+    $(window).scroll(function () {
+        // active nav
+        const scrollPos = $(window).scrollTop();
+        const windowHeight = $(window).height();
+        $('.scrollNav').each(function () {
+            const target = $(this).attr('href');
+            const targetPos = $(target).offset().top - 62;
+            const targetHeight = $(target).outerHeight()
+            $(this).removeClass('active');
+            if (targetPos <= scrollPos && (targetPos + targetHeight) > scrollPos) {
+                $(this).addClass('active');
+            }
+        });
+        // show nav
+        const headerHeight = $('#header').outerHeight()
+        if (headerHeight / 2 <= scrollPos) {
+            $('.navbar').removeClass('d-md-none')
+        } else {
+            $('.navbar').addClass('d-md-none')
+        }
+        // animated
+        $('.animated-up,.animated-right,.animated-left').each(function () {
+            const thisPos = $(this).offset().top;
+            if ((windowHeight - 90 + scrollPos) >= thisPos) {
+                $(this).addClass('fadeIn');
+            }
+        })
+    })
+
 });
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
